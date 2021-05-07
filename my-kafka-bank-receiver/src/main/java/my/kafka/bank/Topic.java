16f6b6e3cb2a2c0d658f57ff7597c26b8c263e23
yao <dong_yao@hotmail.com>
2021-05-07 14:03:13 -0700

    common bank model

--- my-kafka-bank-receiver/src/main/java/my/kafka/bank/Topic.java
+++
@@ -1,8 +0,0 @@
-package my.kafka.bank;
-
-public class Topic {
-
-    // this is the topic to accept bank transactions
-    public static final String TRANSACTION_RAW = "alpha-bank-transactions-raw";
-
-}
