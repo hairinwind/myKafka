4530010f0a388136b4f39add395bf5f9d69309fe
yao <dong_yao@hotmail.com>
2021-05-08 14:25:43 -0700

    my-kafka-spring-stream globalKTable

--- my-kafka-spring-stream/src/main/java/my/kafka/spring/stream/globalktable/GlobalKTablesExample.java
+++ my-kafka-spring-stream/src/main/java/my/kafka/spring/stream/globalktable/GlobalKTablesExample.java
@@ -1,4 +1,4 @@
-package my.kafka.spring.stream.config;
+package my.kafka.spring.stream.globalktable;
 
 import my.kafka.bank.Topic;
 import my.kafka.spring.stream.StateStore;
@@ -10,12 +10,15 @@ import my.kafka.spring.stream.message.Product;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.GlobalKTable;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -24,6 +27,8 @@ import org.springframework.kafka.support.serializer.JsonSerde;
 @Configuration(proxyBeanMethods = false)
 @EnableKafkaStreams
 public class GlobalKTablesExample {
+
+    public static final Logger logger = LoggerFactory.getLogger(GlobalKTablesExample.class);
 
     @Bean
     public KStream<String, EnrichedOrder> alphaBankKStream(StreamsBuilder streamsBuilder) {
@@ -58,6 +63,9 @@ public class GlobalKTablesExample {
 
         JsonSerde<EnrichedOrder> enrichedOrdersSerde = new JsonSerde<>(EnrichedOrder.class);
         enrichedOrdersStream.to(Topic.ENRICHED_ORDER, Produced.with(Serdes.String(), enrichedOrdersSerde));
+
+        Topology topology = streamsBuilder.build();
+        logger.info("topology: {}", topology.describe());
 
         return enrichedOrdersStream;
     }
